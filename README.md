07bfede72568a80505cad1eab52d8b002fbdbfb7
ledwindra <lukman.edwindra@gmail.com>
2019-11-02 23:34:53 +0700

    added and modified files.

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ For detais, go to [wiki](https://github.com/ledwindra/indonesia-stock-exchange/w
 
 ## Intro
 
-...
+This repository aims to get the financial data of publicly listed companies in Indonesia for free. This has only been tested using Python 3.7.3, but it should be fine for any Python 3.x. However, I won't bother testing it using Python 2.x
+
+## Install Requirements
+
+Run the following in the terminal:
+
+```
+pip install -r requirements.txt
+```
 
 ## Add this Repo to Your Machine
+
+Run the following in the terminal:
 
 ```
 git clone https://github.com/ledwindra/indonesia-stock-exchange.git
@@ -14,7 +24,9 @@ git clone https://github.com/ledwindra/indonesia-stock-exchange.git
 
 ## Example
 
-```
-```
+### Daily Trading Summary
 
-## Readings
+Run the following in the terminal:
+```
+python src/get_trading_summary.py [BASE_DATE] [NUM_POOL] [NUM_RETRY] [SLEEP_TIME] [NUM_TIMEOUT]
+```
--- src/get_trade_summary.py
+++ src/get_trade_summary.py
@@ -1,3 +1,5 @@
+# load modules
+import logging
 from json import dump, load
 from requests import get
 from datetime import date, datetime, timedelta
@@ -7,21 +9,36 @@ from sys import argv
 from string import punctuation
 from time import sleep
 
+# log
+logfile = date.today().strftime('%Y%m%d')
+logging.basicConfig(
+    format='%(asctime)s: %(message)s', 
+    filename=getcwd() + '/logs/{}.log'.format(logfile), 
+    level=logging.DEBUG
+)
+
+# create constants
 OUTPUT_PATH = getcwd() + '/output/'
 BASE_URL = 'https://idx.co.id/umbraco/Surface/TradingSummary/GetStockSummary?date={}&start=0&length={}'
+
+# create arguments
 BASE_DATE = argv[1]
 NUM_POOL = cpu_count() - int(argv[2])
 NUM_RETRY = int(argv[3])
 SLEEP_TIME = int(argv[4])
 NUM_TIMEOUT = int(argv[5])
 
-def get_trade_summary():
+def get_urls():
+    "Returns URLs that will be crawled to get daily trading summary.s"
     
+    # open json file which contains publicly listed companies in IDX
     with open(OUTPUT_PATH + 'get-companies.json', 'r') as json_file:
         data = load(json_file)
 
+    # declare base date
     base_date = datetime.strptime(BASE_DATE, '%Y%m%d').date()
     
+    # store URLs for each date
     urls = []
     while base_date <= date.today():
         urls.append(BASE_URL.format(base_date.strftime('%Y%m%d'), len(data)))
@@ -29,10 +46,15 @@ def get_trade_summary():
 
     return urls
 
-def main(urls):
+def get_trade_summary(urls):
+    "Crawls daily trading summary."
 
+    # output file name
     file_name =  urls.translate(str.maketrans('', '', punctuation))
-    response = get(urls, timeout = NUM_TIMEOUT)
+
+    # attempt three times in case of connection timed out
+    # assign time interval betwen attempts
+    # save output in json format
     i = 0
     while i < NUM_RETRY:
         try:
@@ -47,4 +69,5 @@ def main(urls):
 
 if __name__ == "__main__":
     
-    Pool(NUM_POOL).map(main, get_trade_summary())
+    # crawls multiple URLs at the same time
+    Pool(NUM_POOL).map(get_trade_summary, get_urls())
